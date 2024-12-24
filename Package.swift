268b81512e9c7a81bd72364f07176f3a4ce52799
Grant Jarvis <gmj3@rice.edu>
2024-12-24 14:10:37 -0600

    Add VisionOS support

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "RealityCollisions",
-  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
+  platforms: [.macOS(.v10_15), .iOS(.v15), .tvOS(.v13), .visionOS(.v1)],
   products: [
     .library(name: "RealityCollisions", targets: ["RealityCollisions"])
   ],
